e4ab9de536951aa821c89246bc16c3f96131344c
Dijon <dijon.ajeti@network.elancoah.com>
2023-07-12 11:22:22 +0100

    Getting started

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -32,4 +32,25 @@ const config = {
   },
 };
 
-module.exports = config;
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+    'node': true
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaVersion': 12,
+    'sourceType': 'module'
+  },
+  // ...
+  'rules': {
+    'quotes': ['error', 'single'],
+    // we want to force semicolons
+    'semi': ['error', 'never'],
+    // we use 2 spaces to indent our code
+    'indent': ['error', 2],
+    // we want to avoid extraneous spaces
+    'no-multi-spaces': ['error']
+  }
+}
